5f4f5386b7285384c02cdd122b5c364734042e27
naman <naman.khalkuria@juspay.in>
2020-07-05 23:34:35 +0530

    minor fix

---
+++ popup.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  document.querySelector("button").addEventListener('click', function(){
+    chrome.tabs.query({currentWindow: true, active: true},
+                      function(tabs) {
+                        chrome.tabs.sendMessage(tabs[0]);
+
+                      });
+
+  }, false);
+)};
